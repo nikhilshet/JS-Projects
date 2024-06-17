027e77e2c963d83ef0589415df04f4347ba8601c
nikhilshet <64070132+nikhilshet@users.noreply.github.com>
2024-06-17 12:03:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ btn.onclick = myFunc;
 function myFunc(){
 var random1 = Math.ceil(Math.random() * 6);
 var random2 = Math.ceil(Math.random() * 6);
-var imgdice = document.getElementsByClassName("img1")[0].setAttribute("src" , "dice" + random1 + ".png");
-var imgdice = document.getElementsByClassName("img2")[0].setAttribute("src" , "dice" + random2 + ".png");
+document.getElementsByClassName("img1")[0].setAttribute("src" , "dice" + random1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src" , "dice" + random2 + ".png");
 if(random1 > random2){
     document.querySelectorAll("h2")[0].textContent = "Player 1 Wins";
 }else if(random1 < random2){
@@ -14,6 +14,6 @@ if(random1 > random2){
 }else{
     document.querySelectorAll("h2")[0].textContent = "Its A Draww";
 }
-
+}
 
 var head = document.querySelector("h1");
